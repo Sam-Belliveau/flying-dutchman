29b2a4a1ce3e350e3f620f9ac189c8e17e3b5cca
Sam Belliveau <sam.belliveau@gmail.com>
2023-06-04 23:21:35 -0400

    Optimize Counting

--- src/evaluate/pesto/mod.rs
+++ src/evaluate/pesto/mod.rs
@@ -2,7 +2,10 @@ pub mod gamephase;
 pub mod phased_score;
 pub mod psq_table;
 
-use chess::{Board, Color::{White, Black}};
+use chess::{
+    Board,
+    Color::{Black, White},
+};
 
 use self::{gamephase::GamePhase, phased_score::PhasedScore, psq_table::PieceSquareTable};
 
@@ -12,14 +15,21 @@ pub fn evaluate(board: &Board) -> Score {
     let gamephase = GamePhase::new(board);
     let mut score = PhasedScore::new();
 
+    let white_mask = board.color_combined(White);
+    let black_mask = board.color_combined(Black);
+
     for (piece, table) in PieceSquareTable::TABLES {
-        for square in board.color_combined(White) & board.pieces(piece) {
-            score += PhasedScore::from_piece(piece, White);
+        let pieces = board.pieces(piece);
+        let white_pieces = pieces & white_mask;
+        let black_pieces = pieces & black_mask;
+
+        score += PhasedScore::from_piece(piece, White) * white_pieces.popcnt() as Score;
+        for square in white_pieces {
             score += table.get_square(square, White);
         }
 
-        for square in board.color_combined(Black) & board.pieces(piece) {
-            score += PhasedScore::from_piece(piece, Black);
+        score += PhasedScore::from_piece(piece, Black) * black_pieces.popcnt() as Score;
+        for square in black_pieces {
             score += table.get_square(square, Black);
         }
     }
