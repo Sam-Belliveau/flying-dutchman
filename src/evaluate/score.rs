0b918060550120b554e659d2802e90a272410f06
Sam Belliveau <sam.belliveau@gmail.com>
2025-01-14 20:17:34 -0500

    Have a draw be worth a normal amount

--- src/evaluate/score.rs
+++ src/evaluate/score.rs
@@ -9,7 +9,7 @@ pub const MATE: Score = Score::MAX / 16;
 pub const MATE_MOVE: Score = CENTIPAWN;
 pub const MATE_CUTOFF: Score = MATE - 1024 * MATE_MOVE;
 
-pub const DRAW: Score = -MATE;
+pub const DRAW: Score = 0;
 
 pub fn score_mark(score: Score) -> Score {
     // This is how we keep track of how many moves we are from mate.
