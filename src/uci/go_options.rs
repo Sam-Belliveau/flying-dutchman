f3b91bb8bf66c3635630c00dc2022efea4393f92
Sam Belliveau <sam.belliveau@gmail.com>
2025-01-14 20:30:12 -0500

    Allow bot to use more of increment.

--- src/uci/go_options.rs
+++ src/uci/go_options.rs
@@ -114,7 +114,13 @@ impl GoOptions {
                     + 16 * board.pieces(Piece::Queen).popcnt())
                     / 4;
 
-                let time_for_move = (time + inc)
+                let adjusted_inc = inc
+                    .checked_mul(moves_left - 1)
+                    .unwrap_or_default()
+                    .checked_div(moves_left as u32)
+                    .unwrap_or_default();
+
+                let time_for_move = adjusted_inc + time
                     .checked_div(moves_left as u32)
                     .unwrap_or_default()
                     .saturating_sub(BUFFER);
